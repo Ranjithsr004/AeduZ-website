9139c6d8179504e27f5e900f8cc00eb63efeca19
RANJITH S <ranjithsr004@gmail.com>
2024-03-29 15:25:51 +0530

    Footer and Testimonial included

---
+++ app/Component/Footer/Footer.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+
+
+const Footer = () => {
+    return (
+      <footer className="bg-gray-900 text-white py-8 pr-6">
+        <div className="container  mx-4 mr-6  md:mx-4 lg:mx-auto xl:mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div >
+              <img className="h-12 w-auto mb-4" src="https://media.licdn.com/dms/image/D560BAQH6XGSlZuae0Q/company-logo_200_200/0/1708798945374/aeduz_logo?e=1718236800&v=beta&t=mkoKHLYtHQYhXhlQxDVIc7mpwFApukMRJ-ehHiJK9Ms"/>
+              <p className="mb-4">
+                AeduZ is your premier destination for personalized mentorship. Through our innovative platform & network of experienced mentors, we empower the students, filled them with confidence & resilience to overcome challenges & achieve their educational goals.
+              </p>
+            </div>
+            <div className="lg:px-12 ">
+              <h2 className="text-lg font-semibold mb-4">Important Links</h2>
+              <ul>
+                <li className="mb-2">
+                  <a href="#">Home</a>
+                </li>
+                <li className="mb-2">
+                  <a href="#">About Us</a>
+                </li>
+                <li className="mb-2">
+                  <a href="#">Institutions</a>
+                </li>
+                <li className="mb-2">
+                  <a href="#">Contact Us</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="text-lg font-semibold mb-4">Social Media Handles</h2>
+              <div className="flex items-center">
+              <a href="#" className="mr-4">
+                <FaFacebook size="2em" />
+              </a>
+              <a href="#" className="mr-4">
+                <FaInstagram size="2em" />
+              </a>
+              <a href="#" className="mr-4">
+                <FaTwitter size="2em" />
+              </a>
+              <a href="#" className="mr-4">
+                <FaYoutube size="2em" />
+              </a>
+            </div>
+            <p className="border-t border-gray-800 mt-1 pt-8 flex justify-between items-center">Copyright &copy; AeduZ Team 2024</p>
+            <div className="border-t border-gray-800 mt-0 pt-2 flex items-center">
+              <a href="#">Privacy Policy</a>
+              <span className="mx-4">|</span>
+              <a href="#">Terms of Service</a>
+            </div>
+            <p className="mt-2 text-gray-400">⚲ Students Residential Area, IIT Bombay, 400076</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
---
+++ app/Component/Testimonial/Testimonial.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { useState } from 'react';
+import TestiData from '../TestiData/TestiData';
+
+
+const Testimonial = (props) => {
+    let reviews = props.reviews;
+    const [index, setIndex] = useState(0);
+
+    function leftShiftHandler() {
+        if(index - 1 < 0) {
+            setIndex(reviews.length - 1);
+        }
+        else {
+            setIndex(index - 1);
+        }
+    }
+
+    function rightShiftHandler() {
+        if(index + 1 >=reviews.length) {
+            setIndex(0);
+        }
+        else {
+            setIndex(index+1);
+        }
+
+    }
+
+    function surpriseHandler() {
+        let randomIndex =  Math.floor(Math.random() * reviews.length);
+        setIndex(randomIndex);
+    }
+
+  return (
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+    <div className="text-center">
+    <h1 className="lg:text-5xl lg:py-12 md:text-4xl font-bold  ">Testimonials</h1>
+    <div className='w-[85vw] md:w-[700px] bg-white flex flex-col justify-center items-center mt-10 p-10 transition-all duration-700 hover:shadow-xl rounded-md'>
+
+        <TestiData review = {reviews[index]}></TestiData>
+     
+
+      <div className='flex text-3xl mt-10 gap-3 text-blue-400 font-bold mx-auto'>
+        <button 
+        onClick={leftShiftHandler}
+        className='cursor-pointer hover:text-blue-500 '>
+            <FiChevronLeft/>
+        </button>
+        <button 
+        onClick={rightShiftHandler}
+        className='cursor-pointer hover:text-blue-500 '>
+            <FiChevronRight/>
+        </button>
+      </div>
+
+      <div className='mt-6'>
+        <button
+        onClick={surpriseHandler}
+         className='bg-blue-500 hover:bg-blue-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg'>
+            Surprise Me
+        </button>
+      </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Testimonial
