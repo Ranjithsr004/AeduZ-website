37db94e4746bd85fb5d18db39e8a4f113ce63e8d
ArnavUpadhyay7 <arnavupadhyay7@gmail.com>
2024-03-30 13:24:21 +0530

    UPDATED
    Home page button adjusted as well as size of the text

--- app/Home.js/Hero.js
+++ app/Home.js/Hero.js
@@ -10,12 +10,12 @@ function Hero() {
         <div className="md:w-[50%] mx-auto max-w-2xl py-32 sm:py-48 lg:py-48">
           <div animate={{y:0,opacity:1}} initial={{y:-150, opacity:0}} transition={{duration:1.5}} className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-blue-800 sm:text-6xl">
-                <span className='text-2xl md:text-3xl'>Through</span> AeduZ
+                <span className='text-2xl md:text-4xl'>Through</span> AeduZ
             </h1>
             <p className="mt-6 mb-12 text-2xl md:text-4xl  leading-8 text-[#37517e]">
                 Guiding Dreams, Unlocking Potential
             </p>
-              <Link href="CheckOut" className="rounded-md bg-indigo-600 px-6 py-4 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              <Link href="CheckOut" className="rounded-md bg-indigo-600 md:px-10 px-6 md:py-5 py-4 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Book 1:1 call
               </Link>
           </div>
